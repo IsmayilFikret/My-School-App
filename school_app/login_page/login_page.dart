6aface58d38105c36c84e5fc0e3137943ba025b6
IsmayilFikret <ismayilfikretzade9797@gmail.com>
2023-04-21 10:06:21 +0400

    fds

---
+++ school_app/login_page/login_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController repasswordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
