320e1f7b2b936c1309cfb0713ae64f8f1306141d
IsmayilFikret <ismayilfikretzade9797@gmail.com>
2023-04-27 00:07:31 +0400

    df

--- school_app/lib/Home_Page.dart
+++ school_app/lib/Home_Page.dart
@@ -14,49 +14,54 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.deepPurple,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.search),
-          ),
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const SignInPage(),
-                ),
-              );
-            },
-            icon: const Icon(Icons.login),
-          ),
-        ],
-        title: const Text(' MEKTEBLER '),
-      ),
-      body: Container(
-        color: const Color.fromARGB(255, 218, 213, 213),
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: mektebler.length,
-                itemBuilder: (context, index) => Card(
-                  color: Colors.white,
-                  child: ListTile(
-                    onTap: () {},
-                    title: Text(
-                      mektebler[index].keys.first.toString(),
-                    ),
+      appBar: _appbar(context),
+      body: _body(),
+    );
+  }
+
+  Container _body() {
+    return Container(
+      color: const Color.fromARGB(255, 218, 213, 213),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: mektebler.length,
+              itemBuilder: (context, index) => Card(
+                color: Colors.white,
+                child: ListTile(
+                  onTap: () {},
+                  title: Text(
+                    mektebler[index].keys.first.toString(),
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
+    );
+  }
+
+  AppBar _appbar(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      backgroundColor: Colors.deepPurple,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.search),
+        ),
+        IconButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (context) => const SignInPage(),
+            ));
+          },
+          icon: const Icon(Icons.login),
+        ),
+      ],
+      title: const Text(' MEKTEBLER '),
     );
   }
 }
