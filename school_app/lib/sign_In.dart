04df5f8adacc2b8cccdf27f7ee4efc2de504a145
IsmayilFikret <ismayilfikretzade9797@gmail.com>
2023-04-26 22:51:38 +0400

    sd

--- school_app/lib/sign_In.dart
+++ school_app/lib/sign_In.dart
@@ -11,28 +11,6 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.deepPurple,
-        title: const Text('MEKTEBLER'),
-      ),
-      backgroundColor: const Color.fromARGB(255, 218, 213, 213),
-      body: const _Body(),
-    );
-  }
-}
-
-class _Body extends StatefulWidget {
-  const _Body();
-
-  @override
-  State<_Body> createState() => _BodyState();
-}
-
-class _BodyState extends State<_Body> {
   final TextEditingController emailController = TextEditingController();
 
   final TextEditingController passwordController = TextEditingController();
@@ -42,121 +20,128 @@ class _BodyState extends State<_Body> {
   final TextEditingController usernameController = TextEditingController();
 
   bool obscureText = true;
-
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Form(
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBoxWidget(
-                value: 50,
-              ),
-              const Icon(
-                Icons.school,
-                size: 100,
-              ),
-              const SizedBoxWidget(
-                value: 75,
-              ),
-              const Text(
-                'Xoş gəldiniz!',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36),
-              ),
-              const SizedBoxWidget(
-                value: 10,
-              ),
-              const Text(
-                'Zəhmət olmasa giriş edin !',
-                style: TextStyle(fontWeight: FontWeight.normal, fontSize: 20),
-              ),
-              const SizedBoxWidget(
-                value: 50,
-              ),
-              //email textfield
-              CustomTextField(
-                inputype: TextInputType.emailAddress,
-                controller: emailController,
-                hintText: 'Email',
-                autoFocus: true,
-              ),
-              const SizedBoxWidget(
-                value: 15,
-              ),
-              CustomTextField(
-                obscureText: obscureText,
-                icon: obscureText == true
-                    ? Icons.visibility_off
-                    : Icons.visibility,
-                onPress: () {
-                  setState(
-                    () {
-                      obscureText = !obscureText;
-                    },
-                  );
-                },
-                //obscureText: true,
-                controller: passwordController,
-                hintText: 'Password',
-              ),
-              const SizedBoxWidget(
-                value: 20,
-              ),
-              //sign in button
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: InkWell(
-                  onTap: () {},
-                  child: Container(
-                    padding: const EdgeInsets.all(15),
-                    decoration: const BoxDecoration(
-                      color: Colors.deepPurple,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(30),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+        title: const Text('MEKTEBLER'),
+      ),
+      backgroundColor: const Color.fromARGB(255, 218, 213, 213),
+      body: SingleChildScrollView(
+        child: Form(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBoxWidget(
+                  value: 50,
+                ),
+                const Icon(
+                  Icons.school,
+                  size: 100,
+                ),
+                const SizedBoxWidget(
+                  value: 75,
+                ),
+                const Text(
+                  'Xoş gəldiniz!',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36),
+                ),
+                const SizedBoxWidget(
+                  value: 10,
+                ),
+                const Text(
+                  'Zəhmət olmasa giriş edin !',
+                  style: TextStyle(fontWeight: FontWeight.normal, fontSize: 20),
+                ),
+                const SizedBoxWidget(
+                  value: 50,
+                ),
+                //email textfield
+                CustomTextField(
+                  inputype: TextInputType.emailAddress,
+                  controller: emailController,
+                  hintText: 'Email',
+                  autoFocus: true,
+                ),
+                const SizedBoxWidget(
+                  value: 15,
+                ),
+                CustomTextField(
+                  obscureText: obscureText,
+                  icon: obscureText == true
+                      ? Icons.visibility_off
+                      : Icons.visibility,
+                  onPress: () {
+                    setState(
+                      () {
+                        obscureText = !obscureText;
+                      },
+                    );
+                  },
+                  //obscureText: true,
+                  controller: passwordController,
+                  hintText: 'Password',
+                ),
+                const SizedBoxWidget(
+                  value: 20,
+                ),
+                //sign in button
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: InkWell(
+                    onTap: () {},
+                    child: Container(
+                      padding: const EdgeInsets.all(15),
+                      decoration: const BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(30),
+                        ),
                       ),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'Sign In',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold),
+                      child: const Center(
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBoxWidget(
-                value: 25,
-              ),
-              //not a member? register now
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Not a member ? ',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => const RegisterPage(),
-                        ),
-                      );
-                    },
-                    child: const Text(
-                      '   Register now',
-                      style: TextStyle(
-                          color: Colors.blue, fontWeight: FontWeight.bold),
+                const SizedBoxWidget(
+                  value: 25,
+                ),
+                //not a member? register now
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Not a member ? ',
+                      style: TextStyle(fontWeight: FontWeight.bold),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const RegisterPage(),
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        '   Register now',
+                        style: TextStyle(
+                            color: Colors.blue, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
